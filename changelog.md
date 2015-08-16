eb6db729b4bca1926f93cd5b1d1aec9bfdbfc67f
Michael Wohlfahrter <michael_wohlfahrter@hotmail.com>
2015-08-16 18:25:31 +0200

    added default setting for auth type

--- changelog.md
+++ changelog.md
@@ -1,5 +1,9 @@
 # Changelog for wuifdesign/multiauth
 
+## 0.2.0 (2015-08-16)
+
+- Added default setting for auth type
+
 ## 0.1.0 (2015-08-16)
 
 - Initial Version
--- src/MultiAuth.php
+++ src/MultiAuth.php
@@ -7,6 +7,13 @@ use Illuminate\Support\Facades\Route;
 class MultiAuth
 {
     /**
+     * The application instance.
+     *
+     * @var \Illuminate\Foundation\Application
+     */
+    protected $app;
+
+    /**
      * Registered AuthManagers.
      *
      * @var array
@@ -18,8 +25,10 @@ class MultiAuth
      */
     public function __construct(Application $app)
     {
-        foreach ($app['config']['auth.multi'] as $key => $config) {
-            $this->providers[$key] = new AuthManager($app, $key, $config);
+        $this->app = $app;
+
+        foreach ($this->app['config']['auth.multi'] as $key => $config) {
+            $this->providers[$key] = new AuthManager($this->app, $key, $config);
         }
     }
 
@@ -35,7 +44,7 @@ class MultiAuth
             return $this->providers[$authName];
         }
 
-        throw new \Exception('Multi AuthManager "'.$authName.'" not found');
+        throw new \Exception('Multi AuthManager "'.$authName.'" not found!');
     }
 
     /**
@@ -46,12 +55,21 @@ class MultiAuth
      */
     public function __call($name, $arguments = array())
     {
-        $authName = Route::current()->getAction()['auth'];
+        $action = Route::current()->getAction();
+
+        if(isset($action['auth'])) {
+            $authName = $action['auth'];
+        } else {
+            if($this->app['config']['auth.default'] === null) {
+                throw new \Exception('No auth name given and no default setting for MultiAuth found!');
+            }
+            $authName = $this->app['config']['auth.default'];
+        }
 
         if (array_key_exists($authName, $this->providers)) {
             return call_user_func_array(array($this->providers[$authName], $name), $arguments);
         }
 
-        throw new \Exception('Multi AuthManager "'.$authName.'" not found');
+        throw new \Exception('Multi AuthManager "'.$authName.'" not found!');
     }
 }
